bdf36179c83c37c42b2efe1917a80f9c1738d917
pallavi <pallavidighe2@gmail.com>
2019-01-29 16:16:30 +0530

    Assignment

--- Assigmnet 1 Data type and list.py
+++ Assigmnet 1 Data type and list.py
@@ -4,6 +4,96 @@ Assignment
 1 create 5 variable for each data type
 2 create 5 list variable with 3 elements like name,address,contact number
 
-
-
 """
+# Int Data
+
+a = 5
+print(a)
+b = 3
+print(b)
+c = 8
+print(c)
+d = 7
+print(d)
+e = 6
+print(e)
+
+
+# Float Data
+a = 0.5
+print(a)
+b = 3.9
+print(b)
+c = 8.4
+print(c)
+d = 7.2
+print(d)
+e = 6.9
+print(e)
+
+e=int(e)
+print(e)
+
+# String Data
+a = "Pallavi"
+print(a)
+b = "Samrthview"
+print(b)
+c = "Python"
+print(c)
+d = "Assignment"
+print(d)
+e = "Class"
+print(e)
+
+# None
+
+A = None
+print(A,type(A))
+B = None
+print(A,type(B))
+C = None
+print(C,type(C))
+D = None
+print(D,type(D))
+E = None
+print(E,type(E))
+
+# Boolean
+A = True
+print(A)
+B = False
+print(B)
+
+# List
+
+List1 = ["Pallavi", "Tanish Samruddhi" ,12345]
+print(List1)
+
+List2 = ["Sonali","Dighe",1,45,"Python"]
+print(List2)
+
+List3 = ["Somal","Home",1452,3.5,6.5]
+print(List3)
+
+List4 = ["Samarthview","Python","Batch","January",2019]
+print(List4)
+
+List5 = ["New","Python","Batch","class","Assignment",1]
+print(List5)
+
+print(List5[2])
+print(List1+List2)
+print(List1*3)
+
+List6 = ["Python",["Samrthvieew",1,2],["Pallavi",["Sonali",["Dighe",[123]]]]]
+print(List6)
+print(List6[2][1][1][1][0])
+
+
+
+
+
+
+
+
